5bfda24d8e85b1e0085c247d4395d01a36ce4a26
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-12-20 20:20:42 +0530

    Time: 11 ms (97.25%) | Memory: 24.3 MB (93.30%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         vector<int> ans(n,1);
         int curr = 1;
-        for(int i = 0; i < n; i++) {
-            ans[i] *= curr;
-            curr *= nums[i];
+
+        for(int i = 1; i < n; i++) {
+            ans[i]=nums[i-1]*ans[i-1];
         }
         curr = 1;
         for(int i = n - 1; i >= 0; i--) {
