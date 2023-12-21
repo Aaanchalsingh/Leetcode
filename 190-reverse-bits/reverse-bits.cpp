8dc14e5b7bcfb6cd272ebae32b99ab5f34976ad1
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-12-21 20:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (58.98%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,7 +3,8 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans=0;int i=1,result=0;
         for(int i=0;i<32;i++){
-            ans=(n&1)<<31-i;
+            if(n&1)
+            ans=1<<31-i;
             result=result|ans;
             n=n>>1;
         }
