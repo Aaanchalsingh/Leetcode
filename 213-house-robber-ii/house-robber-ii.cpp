7850170e2c546c2a1be4f34dcb9bc2702b43573e
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-12-22 23:05:57 +0530

    Time: 4 ms (29.20%) | Memory: 8.2 MB (71.12%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -3,12 +3,11 @@ public:
     int rob(vector<int>& v) {
         int n=v.size();vector<int> dp(n+1,0),ans;
         if(n==1) return v[0];
-        for(int i=0;i<n;i++) if(i!=0) ans.push_back(v[i]);
+        for(int i=0;i<n-1;i++) ans.push_back(v[i]);
         for(int i=2;i<=n;i++) dp[i]=max(dp[i-1],ans[i-2]+dp[i-2]);
         int result=dp[n];
-        ans.clear();
-        for(int i=0;i<n-1;i++) ans.push_back(v[i]);
-
+        ans.push_back(v[n-1]);
+        ans.erase(ans.begin());
         fill(dp.begin(),dp.end(),0);
         for(int i=2;i<=n;i++) dp[i]=max(dp[i-1],ans[i-2]+dp[i-2]);
 
