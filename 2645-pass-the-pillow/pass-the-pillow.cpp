1ab78c3b18bd900245f26da7b9205d8412282050
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-04 21:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (77.11%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int i=1,ind=0; bool flag=false;
-        int k=0;
-        while(k<time){
+        int i=1;bool flag=false;
+        while(time--){
             if(i==1) flag=false;
             if(i==n) flag=true;
             if(!flag) i++;
             else i--;
-            k++;
         }
         return i;
     }
