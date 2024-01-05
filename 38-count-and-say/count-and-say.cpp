94eacb0859dd3b885d632dbb6342850e4ab87289
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-05 15:36:09 +0530

    Time: 7 ms (23.98%) | Memory: 7.1 MB (59.54%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string count(string s){
+        int n=s.size();
+        int c=1;
+        string ans="";
+        for(int i=0;i<n-1;i++){
+            if(s[i]!=s[i+1]){
+                ans+=to_string(c)+s[i];
+                c=1;
+            }
+            else c++;
+        }
+        if(s[n-1]!=s[n-2]) ans+=to_string(1)+s[n-1];
+        else if(c>1) ans+=to_string(c)+s[n-1];
+        cout<<ans<<endl;
+        return ans;
+    }
+    string countAndSay(int n) {
+        string s="1";
+        if(n>=2) {s="11";n--;}
+        while(--n){
+            s=count(s);
+        }
+        return s;
+    }
+};
