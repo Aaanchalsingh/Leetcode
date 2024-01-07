2ca761d40cfc0739b0269c6b4fcdd649c86a78a8
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-07 12:13:31 +0530

    Time: 8 ms (100.00%) | Memory: 25.4 MB (60.00%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -4,10 +4,11 @@ public:
         double n=v.size(),cur=0,area=-1,k=0,temp=0;
         for(int i=0;i<n;i++){
             temp=sqrt((v[i][0]*v[i][0])+(v[i][1]*v[i][1]));
-            if(cur<temp||(cur==temp && v[i][0]*v[i][1] > area)){
-                cur=temp;
+            if(cur<=temp){
                 k=v[i][0]*v[i][1];
-                area=k;
+                if(cur==temp) area=max(area,k);
+                else area=k;
+                cur=temp;
             }
         }
         return area;
