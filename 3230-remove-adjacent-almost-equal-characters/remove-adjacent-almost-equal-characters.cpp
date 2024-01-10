29f1c4626a155e728777b1f694d037d4a39987e0
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-10 15:57:38 +0530

    Time: 3 ms (37.15%) | Memory: 6.5 MB (99.75%) - LeetSync

--- 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.cpp
+++ 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.cpp
@@ -4,8 +4,7 @@ public:
         int n=word.size(),count=0;
         for(int i=0;i<n-1;i++){
             if(word[i]==word[i+1]||word[i]==word[i+1]-1||word[i]==word[i+1]+1){
-                cout<<word[i]<<" "<<word[i+1]<<endl;
-                word[i+1]='/';
+                cout<<word[i]<<" "<<word[i+1]<<endl;i++;
                 count++;
             }
         }
