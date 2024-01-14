f8c304ceff4baa1afb0b9d3dbe6ea4cd4640d815
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-14 20:40:08 +0530

    Time: 1759 ms (56.18%) | Memory: 262.7 MB (82.39%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int maximizeSquareArea(int m, int n, vector<int>& h, vector<int>& v) {
-    long long ans = 0, mod= 1e9 + 7;
-    unordered_set<int> st;
-    h.push_back(1); h.push_back(m); 
-    v.push_back(1); v.push_back(n); 
-    for(int i = 0; i < h.size(); ++i){
-        for(int j = 0; j < h.size(); ++j){
-            st.insert(abs(h[i] - h[j]));
-        }
-    }
-    for(int i = 0; i < v.size(); ++i){
-        for(int j = 0; j < v.size(); ++j){
-            if(i != j  && st.count(abs(v[i] - v[j]))) {
-                ans = max(ans, (long long)abs(v[i] - v[j]));
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        long long ans=0,mod=1e9+7;
+        hFences.push_back(1);
+        hFences.push_back(m);
+        vFences.push_back(1);
+        vFences.push_back(n);
+        unordered_set<int> st;
+        for(int i=0;i<hFences.size();i++){
+            for(int j=0;j<hFences.size();j++){
+                st.insert(abs(hFences[i]-hFences[j]));
             }
         }
+        for(int i=0;i<vFences.size();i++){
+            for(int j=0;j<vFences.size();j++){
+                if(st.count(abs(vFences[i]-vFences[j]))){
+                    long long diff=abs(vFences[i]-vFences[j]);
+                    ans=max(ans,diff);
+                }
+            }
+        }
+        return ans>0? (ans*ans)%mod:-1;
     }
-    return (ans == 0)?-1: ((ans * ans) % mod);
-}
 };
