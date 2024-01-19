a4a88af285174b71146e5ffb3a0dcdab98f7cebc
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-19 14:50:14 +0530

    Time: 102 ms (95.39%) | Memory: 89 MB (38.58%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -3,7 +3,6 @@ public:
     int minOperations(vector<int>& v, int k) {
         int ans = 0, n = v.size(), count = 0;
         for (int i = 0; i < n; i++) ans ^= v[i];
-        if (k <= 0) return __builtin_popcount(ans);
         while (k > 0 || ans > 0) {
             if ((k & 1) != (ans & 1)) count++;
             k >>= 1;
