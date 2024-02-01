dec58388f5f8feb3e7a671a8db877901f396a5c2
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-01 15:32:14 +0530

    Time: 3 ms (96.71%) | Memory: 11.9 MB (25.85%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int solve(string x, string y, int m, int n, vector<vector<int>>& dp) {
-        if (m < 0 and n < 0)
-            return 0;
-        if (m <= 0 or n <= 0) {
-            return abs(m - n);
-        }
-        if (dp[m][n] != -1)
-            return dp[m][n];
-        if (x[m - 1] == y[n - 1])
-            return dp[m][n] = solve(x, y, m - 1, n - 1, dp);
-        return dp[m][n] = min({1 + solve(x, y, m - 1, n, dp),
-                               1 + solve(x, y, m, n - 1, dp),
-                               1 + solve(x, y, m - 1, n - 1, dp)});
-    }
     int minDistance(string w1, string w2) {
-        int m = w1.size();
-        int n = w2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return solve(w1, w2, w1.size(), w2.size(), dp);
+        int m = w1.size(), n = w2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = i;
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (w1[i - 1] == w2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] =
+                        1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+            }
+        }
+        return dp[m][n];
     }
 };
