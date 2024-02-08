5961acf56f7d4ed69eee0c7481cdae780b5370c0
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-08 20:35:38 +0530

    Time: 26 ms (95.51%) | Memory: 10.6 MB (32.42%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numSquares(int n) {
-        std::vector<int> dp(n + 1, 0);
+        vector<int> dp(n + 1, 0);
         dp[0] = 0;
         dp[1] = 1;
 
@@ -9,7 +9,7 @@ public:
             int min_squares = INT_MAX;
             for (int j = 1; j * j <= i; ++j) {
                 int remaining = i - j * j;
-                min_squares = std::min(min_squares, dp[remaining]);
+                min_squares = min(min_squares, dp[remaining]);
             }
             dp[i] = min_squares + 1;
         }
