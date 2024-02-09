daf4a3cb74f799f659bffecc3edfc9d82f6ea3fd
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-09 22:53:47 +0530

    Time: 11 ms (67.37%) | Memory: 14.8 MB (5.58%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,44 +1,24 @@
-
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& A) {
-        int m = A.size();
-        int n = A[0].size();
-
-        if (m == 1 || n == 1) return A[0][0];
-
-        vector<vector<int>> dp(m,vector<int>(n, INT_MAX));
-        int ans = INT_MAX;
-
-        for (int i = 0; i < A.size(); ++i) {
-            ans = min(ans, minFallingPathSum(A, 0, i, dp));
+    int solve(int i, int j, int n, vector<vector<int>>& v,
+              vector<vector<int>>& dp) {
+        if (j < 0 or j >= n)
+            return 1e9;
+        if (dp[i][j] != 1e9)
+            return dp[i][j];
+        if (i == n - 1)
+            return dp[i][j]=v[n - 1][j];
+        return dp[i][j] = min({solve(i + 1, j - 1, n, v, dp),
+                               solve(i + 1, j, n, v, dp),
+                               solve(i + 1, j + 1, n, v, dp)}) +
+                          v[i][j];
+    }
+    int minFallingPathSum(vector<vector<int>>& v) {
+        int n = v.size(), ans = 1e9;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 1e9));
+        for (int j = 0; j < n; j++) {
+            ans = min(ans, solve(0, j, n, v, dp));
         }
-
         return ans;
     }
-
-    int minFallingPathSum(vector<vector<int>>& A, int row, int col,  vector<vector<int>>& dp) {
-        int m = A.size();
-        int n = A[0].size();
-
-        if (dp[row][col] != INT_MAX) return dp[row][col];
-
-        if (row == m - 1)
-            return dp[row][col] = A[row][col];
-
-        int left = INT_MAX, right = INT_MAX;
-
-        if (col > 0)
-            left = minFallingPathSum(A, row + 1, col - 1, dp);
-
-        int straight = minFallingPathSum(A, row + 1, col, dp);
-
-        if (col < n - 1)
-            right = minFallingPathSum(A, row + 1, col + 1, dp);
-
-        dp[row][col] = min(left, min(straight, right)) + A[row][col];
-
-        return dp[row][col];
-    }
 };
-
