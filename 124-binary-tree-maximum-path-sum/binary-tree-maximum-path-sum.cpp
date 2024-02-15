37f4f177c2163941e390b5e119c32e2da7df792b
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-15 10:13:59 +0530

    Time: 19 ms (64.71%) | Memory: 26 MB (90.92%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int ans=-1e9;
-    int solve(TreeNode* root)
-    {
-        if(!root)return 0;
-        int l=max(solve(root->left),0);
-        int r=max(solve(root->right),0);
-        int new_price=root->val+l+r;
-        ans=max(ans,new_price);
-        return root->val+max(l,r);
+    int solve(TreeNode* root){
+        if(!root) return 0;
+        int left=max(solve(root->left),0);
+        int right=max(solve(root->right),0);
+        ans=max(ans,left+right+root->val);
+        return root->val+max(left,right);
     }
     int maxPathSum(TreeNode* root) {
         solve(root);
