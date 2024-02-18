8c617462e7ebbd420e7c8b91350cf7a89bfffdd1
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-18 15:47:59 +0530

    Time: 279 ms (5.18%) | Memory: 73.6 MB (38.51%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    bool poss(int ind, vector<int> v) {
-        long long n = v.size(), curr = 0, prev = v[0];
-        if(v[0]>ind) return false; 
-        for (int i = 1; i < n; i++) {
-            curr = ind - prev;
-            prev = v[i] - curr;
-            if (prev > ind)
-                return false;
-        }
-        return true;
-    }
     int minimizeArrayValue(vector<int>& v) {
-        long long low = 0, high = *max_element(v.begin(), v.end()), mid = 0,
-            ans = high;
-        while (low < high) {
-            mid = (low + high) / 2;
-            if (poss(mid, v)) {
-                high = mid;
-            } else
-                low = mid + 1;
+        long long n = v.size(), sum =0, ans = 0, lcm = 0;
+        for (int i = 0; i < n; i++) {
+            sum += v[i];
+            lcm = (sum + i) / (i + 1);
+            ans = max(ans, lcm);
+            cout<<ans<<endl;
         }
-        return high;
+        return ans;
     }
 };
