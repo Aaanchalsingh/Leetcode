395c328cd8bd87c2255e507739543a6dde1f77de
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-25 21:08:03 +0530

    Time: 3 ms (100.00%) | Memory: 23.8 MB (80.00%) - LeetSync

--- 3324-split-the-array/split-the-array.cpp
+++ 3324-split-the-array/split-the-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& v) {
-        vector<int> count(101,0);
-        for(auto x:v) count[x]++;
-        for(auto x:count){
-            if(x<0 || x>2) return false;
+        vector<int> count(101, 0);
+        for (auto x : v) {
+            count[x]++;
+            if (count[x] > 2)
+                return false;
         }
         return true;
     }
