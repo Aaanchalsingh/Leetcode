59aae21cb84ca4c203c6263794ae5bf5ccca6713
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-08 20:36:29 +0530

    Time: 126 ms (85.39%) | Memory: 35.7 MB (92.78%) - LeetSync

--- 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int dfs(int root, int par, int dist, vector<vector<pair<int, int>>>& graph,int& signalSpeed) {
-        int count = (dist % signalSpeed)? 0: 1; 
-        for (auto [n, w] : graph[root]) {
+    int dfs(int root, int par, int dist, vector<vector<pair<int, int>>>& graph, int& signalSpeed) {
+        int count = (dist % signalSpeed) ? 0 : 1; 
+        for (const pair<int, int>& neighbor : graph[root]) {
+            int n = neighbor.first;
+            int w = neighbor.second;
             if (n != par)
                 count += dfs(n, root, dist + w, graph, signalSpeed);
         }
         return count;
     }
 
-    vector<int> countPairsOfConnectableServers(vector<vector<int>>& v,int signalSpeed) {
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& v, int signalSpeed) {
         vector<vector<pair<int, int>>> graph(v.size() + 1);
-        for (auto x : v) {
-            graph[x[0]].push_back({x[1], x[2]});
-            graph[x[1]].push_back({x[0], x[2]});
+        for (const vector<int>& x : v) {
+            int node1 = x[0];
+            int node2 = x[1];
+            int weight = x[2];
+            graph[node1].push_back(make_pair(node2, weight));
+            graph[node2].push_back(make_pair(node1, weight));
         }
 
         vector<int> ans;
-        for (int i = 0; i <= v.size();++i) { 
+        for (int i = 0; i <= v.size(); ++i) { 
             int nodeCount = 0, res = 0;
-            for (auto [n, w] :graph[i]) { 
+            for (const pair<int, int>& neighbor : graph[i]) { 
+                int n = neighbor.first;
+                int w = neighbor.second;
                 int t = dfs(n, i, w, graph, signalSpeed);
                 res += nodeCount * t;
                 nodeCount += t; 
