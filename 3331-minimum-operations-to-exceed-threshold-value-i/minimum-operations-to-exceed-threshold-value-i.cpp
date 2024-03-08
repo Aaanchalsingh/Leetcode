14946936cf56e3b2782fb185717354a5f4c39c58
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-08 20:04:17 +0530

    Time: 8 ms (37.39%) | Memory: 23.8 MB (39.89%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minOperations(vector<int>& v, int k) {
         sort(v.begin(),v.end());
-        int ind=lower_bound(v.begin(),v.end(),k)-v.begin();
-        return ind;
+        return lower_bound(v.begin(),v.end(),k)-v.begin();
     }
 };
