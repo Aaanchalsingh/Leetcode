277b6af20e7724fb3674f3880e753d400958fcb2
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-04-06 07:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (60.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -11,7 +11,6 @@ public:
         for(int i=0;i<s.size();i++){
             if(s[i]=='('||s[i]=='['||s[i]=='{') st.push(s[i]);
             else if(st.size()>0&&same(st.top(),s[i])){
-                if(st.size()==0) return false;
                 st.pop();
             }
             else return false;
