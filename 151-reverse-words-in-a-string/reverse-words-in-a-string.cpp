ffab245cae4208d1c5db091b296ae777d2d000d0
Prince Kumar <aanchalsinghuk@gmail.com>
2024-12-01 12:31:09 +0530

    Time: 15 ms (9.40%) | Memory: 9.8 MB (62.86%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void trimWhitespaces(string &s){
+        int n= s.size();
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' ') break;
+            s.erase(i,i+1);
+            i--;
+        }
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]!=' ') break;
+            s.erase(i,i);
+        }
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                int j=i+1;
+                while(s[j]==' '){
+                    s.erase(j,1);
+                    cout<<"here";
+                }
+                i=j;
+            }
+        }
+        cout<<"["<<s<<"]";
+    }
+    string reverseWords(string s) {
+        int n = s.size(),start=0,last=0;
+        trimWhitespaces(s);
+         reverse(s.begin(),s.end());
+        for(int i = 0;i < n;i++){
+            if(s[i] == ' '){
+                reverse(s.begin()+start,s.begin()+i);
+                start=i+1;
+            }
+        }
+        reverse(s.begin()+start,s.end());
+        return s;
+    }
+};
