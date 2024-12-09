8fb7bc5ff45e189be5428e39a2ab62ee6c7e6e8d
Prince Kumar <aanchalsinghuk@gmail.com>
2024-12-09 08:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (11.11%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = v.size();
         vector<int> result(n,0);
         for(int i=0;i<n;i++){
-            if(v[i]<0){
+            if(v[i] + i<0){
                 result[i]=v[(n+(i+v[i])%n)%n];
             }
             else result[i]=v[(v[i]+i)%n];
